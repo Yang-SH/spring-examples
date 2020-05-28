dc728aede00570a4ab79fe2c1e5fbca937c6744d
y7398 <y73986447@gmail.com>
2020-05-28 21:51:49 +0800

    添加spring监控的自定义健康检查RocketMQHealthIndicator， 添加方法访问次数监控的CounterService

--- spring-boot-hello/src/main/java/com/doudou/web/HelloController.java
+++ spring-boot-hello/src/main/java/com/doudou/web/HelloController.java
@@ -1,13 +1,20 @@
 package com.doudou.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private CounterService counterService;
+
     @RequestMapping("/hello")
     public String index(){
+        counterService.increment("didispace.hello.count");
+
         return "Hello World";
     }
 
