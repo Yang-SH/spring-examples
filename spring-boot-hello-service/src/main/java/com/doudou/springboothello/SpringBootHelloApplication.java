72a96e8cdddbb6534e12a9fc06e4ed7e27f33026
ysh <y73986447@gmail.com>
2020-05-29 17:34:42 +0800

    添加项目

--- spring-boot-hello-service/src/main/java/com/doudou/springboothello/SpringBootHelloApplication.java
+++ spring-boot-hello-service/src/main/java/com/doudou/springboothello/SpringBootHelloApplication.java
@@ -2,10 +2,13 @@ package com.doudou.springboothello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.doudou.web"})
 @SpringBootApplication
+//  激活 Eureka 中的DiscoveryClient实现（自动化配置，创建DiscoveryClient接口针对Eureka客户端的EurekaDiscoveryClient实例）
+@EnableDiscoveryClient
 public class SpringBootHelloApplication {
 
     public static void main(String[] args) {
